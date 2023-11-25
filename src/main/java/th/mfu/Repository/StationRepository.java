285518c8fa6c0bb50ae901781e8349d90b81e2d8
Trin-Rattanasiri <122963183+Trin-Rattnasiri@users.noreply.github.com>
2023-11-25 17:02:32 +0000

    เน

--- src/main/java/th/mfu/Repository/StationRepository.java
+++
@@ -1,9 +0,0 @@
-package th.mfu.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import th.mfu.Domain.Station;
-
-public interface StationRepository extends JpaRepository<Station, Long> {
-    Station findAllByNameAndTime(String name,String time);
-
-}
